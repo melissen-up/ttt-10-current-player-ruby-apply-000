23d5f1050dee5b0cb817e0092b34389bcd9e3d72
Melissa F <melissa.faulner@gmail.com>
2020-10-07 22:45:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |token|
-  if (token == "X") || (token == "O")
+  if token == "X" || token == "O"
     counter =+ 1
   end
 end
-return counter
+counter
 end
